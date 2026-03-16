a71889c46b4ff52d6e7b375fd19c457e71fb134c
Tanveer Alam <ansaritaz78601@gmail.com>
2026-03-16 12:03:00 +0530

    implemented ther posthog using the posghogAI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
 import Navbar from "@/components/Navbar";
+import { PostHogProvider } from "./providers";
+import PostHogPageView from "./PostHogPageView";
+import { Suspense } from "react";
 
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
@@ -33,24 +36,29 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
-        <Navbar />
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-              <LightRays
-                  raysOrigin="top-center-offset"
-                  raysColor="#5dfeca"
-                  raysSpeed={0.5}
-                  lightSpread={0.9}
-                  rayLength={1.4}
-                  followMouse={true}
-                  mouseInfluence={0.02}
-                  noiseAmount={0.0}
-                  distortion={0.01}
-              />
-          </div>
+        <PostHogProvider>
+          <Suspense fallback={null}>
+            <PostHogPageView />
+          </Suspense>
+          <Navbar />
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+                <LightRays
+                    raysOrigin="top-center-offset"
+                    raysColor="#5dfeca"
+                    raysSpeed={0.5}
+                    lightSpread={0.9}
+                    rayLength={1.4}
+                    followMouse={true}
+                    mouseInfluence={0.02}
+                    noiseAmount={0.0}
+                    distortion={0.01}
+                />
+            </div>
 
-          <main>
-            {children}
-          </main>
+            <main>
+              {children}
+            </main>
+        </PostHogProvider>
       </body>
     </html>
   );
